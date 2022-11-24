8dce9c64b8b911c600ab09bf9695c0d3fd05c587
Tesseract22 <caleb2222@qq.com>
2022-11-24 22:50:14 +0000

    change ui, add new textaread functionality

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,29 @@
+jQuery(function() {
+  $("#addButton").on('click', function() {
+      addMarkDownArea();
+  });
+});
+
+
+
+var MARKDOWN_AREAS = {};
+
+
+
 function genNonDuplicateID(){
   return Math.random().toString();
+}
+
+
+function addMarkDownArea() {
+
+  // const  = genNonDuplicateID();
+  // var new_textbox = $(`<div class="row"><textarea class="textbox"></textarea></div>`);
+  var new_textbox = document.createElement("textarea");
+  $("#container").append(new_textbox);
+  console.log($(".row:last-child").first()[0]);
+  var simplemde = new SimpleMDE({
+    element: new_textbox
+  });
+
 }
